83b3b3ec71dee2a09be0640a8e2afe5c2c7712c9
Blanquette0315 <tjfwn0315@gmail.com>
2025-01-10 13:00:17 +0900

    초기 세팅하기
    > gitignore 추가
    > main 함수 만들기
    > pch.h (미리 컴파일 된 헤더) 설정

---
+++ Project/Project/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+
+#include <random>
+
+using namespace std;
